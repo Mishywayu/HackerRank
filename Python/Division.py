5b299b2a61a3e000c9c6fa28018425e38c19b3b6
Mishywayu <michellewayua2@gmail.com>
2024-11-26 00:07:46 +0300

    Task 4:Division

---
+++ Python/Division.py
@@ -0,0 +1,19 @@
+# The provided code stub reads two integers, a and b, from STDIN.
+
+# Add logic to print two lines. The first line should contain the result of integer division,  a//b . 
+# The second line should contain the result of float division,  a/b .
+
+# ROUGH
+a = 20
+b = 10
+
+print (a // b)
+print ((a / b))
+
+# SOLUTION
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a // b)
+    print(a / b)
